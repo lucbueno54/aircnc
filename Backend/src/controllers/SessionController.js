cd2d73f85814b0b51839e68996ac2947fa4d5a1f
lucbueno54 <lucbueno54@outlook.com>
2020-04-10 23:47:46 -0300

    Add frontend React

--- Backend/src/controllers/SessionController.js
+++ Backend/src/controllers/SessionController.js
@@ -2,13 +2,13 @@ const User = require('../model/User')
 
 module.exports = {
     async store(req, res) {
-        let email = req.body.email;
+        let {email} = req.body;
 
         let user = await User.findOne({email});
 
         if(!user)
         {
-           user = await User.create({ email });
+           user = await User.create({email});
         }
         return res.json(user);
     }
